b8ace93557321ce6011d31b9dae32ec535cdc002
jgmsilva <jgui.silva1510@gmail.com>
2023-01-20 13:12:24 -0300

    bumped iced to 0.7 and fixed nix run issue

--- flake.nix
+++ flake.nix
@@ -1,31 +1,44 @@
 {
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-  inputs.dream2nix.url = "github:nix-community/dream2nix";
+  inputs.dream2nix = {
+    url = "github:nix-community/dream2nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
   inputs.nixgl.url = "github:guibou/nixGL";
-  outputs = {self, nixpkgs, dream2nix, rust-overlay, nixgl}:
-    let 
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, dream2nix, rust-overlay, nixgl, flake-utils }:
+    let
       system = "x86_64-linux";
       toolchain = rust-overlay.packages.${system}.rust;
       pkgs = nixpkgs.legacyPackages.${system};
       GLAdapter = nixgl.packages.${system}.nixGLIntel;
-    in dream2nix.lib.makeFlakeOutputs {
+    in
+    let d2n = dream2nix.lib.makeFlakeOutputs rec {
       systemsFromFile = ./nix_systems;
+      autoProjects = true;
       config.projectRoot = ./.;
       source = ./.;
-      packageOverrides = rec {
-      # for build-rust-package builder
+      packageOverrides = {
+        # for build-rust-package builder
         "^.*".set-toolchain.overrideRustToolchain = old: {
           cargo = toolchain;
           rustc = toolchain;
         };
         "^.*".add-pre-build-steps = {
-          #nativeBuildInputs = old: old ++ (with pkgs; [cmake gnumake]);
-          buildInputs = old: old ++ (with pkgs.xorg;[libX11 libXcursor libXrandr libXi libXaw libXft libXmu libXrender libXt libxkbfile ])
-                                                     ++ [GLAdapter pkgs.rust-analyzer pkgs.fontconfig];
+          nativeBuildInputs = old: old ++ (with pkgs; [ cmake pkg-config ]);
+          buildInputs = (with pkgs.xorg;[ libX11 libXcursor libXrandr libXi libXaw libXft libXmu libXrender libXt libxkbfile ]) ++ [ pkgs.fontconfig ];
         };
-        LD_LIBRARY_PATH = "/home/joao/.config/nixpkgs";
       };
-      settings = [  {devShells.env = [{name = "TESTING_VAR"; value = "/home/"; }];} ];
     };
+    in
+    dream2nix.lib.dlib.mergeFlakes [
+      d2n
+      rec {
+        devShells.${system}.default = d2n.devShells.${system}.default.overrideAttrs (old: { buildInputs = old.buildInputs ++ [ pkgs.rust-analyzer GLAdapter ]; });
+        script = pkgs.writeShellScriptBin "script.sh" "${GLAdapter}/bin/nixGLIntel ${d2n.packages.${system}.sys-dashboard}/bin/sys-dashboard";
+        apps.${system}.default = flake-utils.lib.mkApp { drv = script; };
+      }
+    ];
+
 }
